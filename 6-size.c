125d5f001c604b206a3c83ba0ace7358505be76c
Godwin Daniel <lemuelgodwin2@gmail.com>
2023-06-17 08:34:46 +0100

    printf

---
+++ 6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    printf("Size of char is: %zu", sizeof(char));
+    printf("Size of integer is: %zu", sizeof(int));
+    return (0);
+}
